213639e33f80f8998759aacb3e0d94eb2d9f82fe
Brie Cheese <bldresser7@gmail.com>
2020-03-25 22:25:51 -0700

    dynamic kname & copyright

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
     <footer class = "footer container text-center pb-5">
-        <p>&copy; Knowledge Base. Powered by Awesome KB.</p>
+        <p>&copy; <?php echo get_bloginfo( 'name' );?><?php echo comicpress_copyright(); ?>. Powered by Awesome KB.</p>
     </footer>
 
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
